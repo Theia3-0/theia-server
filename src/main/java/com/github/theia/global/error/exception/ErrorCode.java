111428faa1d4fb31db596a84caf0b3f70cad3ad1
신희성 <vvwv3092@gmail.com>
2024-02-20 22:50:00 +0900

    Update ErrorCode

--- src/main/java/com/github/theia/global/error/exception/ErrorCode.java
+++ src/main/java/com/github/theia/global/error/exception/ErrorCode.java
@@ -9,10 +9,10 @@ public enum ErrorCode {
 
     EXPIRED_JWT(401, "Expired Jwt"),
     INVALID_JWT(401, "Invalid Jwt"),
-    DUPLICATE_USER(400, "중복된 유저입니다."),
-    NOT_FOUND_USER(404, "유저를 찾을 수 없습니다."),
+    DUPLICATE_USER(400, "Duplicated User"),
+    NOT_FOUND_USER(404, "Not Found User"),
 
-    ERROR_FEIGN(500, "카카오 유저 정보를 불러오던 중 오류가 발생했습니다");
+    ERROR_FEIGN(500, "Kakao Feign Error");
 
     private final int status;
     private final String message;
